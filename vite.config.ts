a47091f57aa77228ce00c7d8b3f2e07e4a3c9c83
wangfukai <wangfukai@codemao.cn>
2021-05-21 16:59:40 +0800

    fix: test pxtorem

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import pxtorem from 'postcss-pxtorem'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
-    postcss: {}
+    postcss: {
+      plugins: [
+        pxtorem({
+          rootValue: 750
+        })
+      ]
+    }
   },
   plugins: [reactRefresh()]
 })
